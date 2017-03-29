0ec64dc1edfe0cdbb921923dec1d394413b4ea16
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-03-28 17:23:22 -0700

    chapter 10

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ int main()
 
     hitable *list[5];
 
-    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
     list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
     list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.1));
     list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
@@ -42,7 +42,7 @@ int main()
 
     hitable *world = new hitable_list(list, 5);
 
-    camera cam;
+    camera cam(vec3(-2, 2, 1), vec3(0,0,-1), vec3(0, 1, 0), 30, float(nx)/float(ny));
 
     for(int j = ny - 1; j >=0; j--) {
         for(int i = 0; i < nx; i++) {
